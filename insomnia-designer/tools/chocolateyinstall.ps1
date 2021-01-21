37bf8fc77bb17015b1cbbb2dc4eacacf786a8aed
zverev iv <mail@zverev-iv.me>
2021-01-21 14:26:09 +0300

    init

---
+++ insomnia-designer/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  installerType = 'exe'
+  url           = 'https://github.com/Kong/insomnia/releases/download/designer%402020.5.2/Insomnia.Designer-2020.5.2.exe'
+  checksum      = 'A250E2565CDB81070F44C5BDDB1918F4E07ED6FD6EC6D7DE915DBEFA728D1F5F'
+  checksumType  = 'sha256'
+  silentArgs    = '--silent'
+  validExitCodes= @(0)
+  softwareName   = 'Insomnia Designer'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ insomnia-designer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:ChocolateyPackageName
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\insomnia-designer\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Insomnia at $Env:USERPROFILE\AppData
+Remove-Item "$Env:USERPROFILE\AppData\Local\insomnia-designer" -recurse
+Remove-Item "$Env:USERPROFILE\AppData\Roaming\Insomnia Designer" -recurse
